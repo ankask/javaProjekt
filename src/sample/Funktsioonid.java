ea229572181d7c5afebce084e2b3b0214d3a5387
Annika <anenno@ttu.ee>
2017-04-19 00:14:54 +0300

    5. katse

--- src/sample/Funktsioonid.java
+++ src/sample/Funktsioonid.java
@@ -6,16 +6,17 @@ import java.util.ArrayList;
 public class Funktsioonid extends Main {
 
 
-    //muutujad stringid ja double-id (et ka komadega arvestaks)
-    String nimi;
+    //Muutujad: stringid ja double-id (et ka komadega arvestaks)
 
+    String nimi;
     Double neto = 0.0; //Algväärtus 0. Kui pole teada, siis saab sisestada 0 või tühjaks jätta. Muul juhul number.
     Double bruto = 0.0; //Algväärtus 0. Kui pole teada, siis saab sisestada 0 või tühjaks jätta. Muul juhul number.
-    Double fondid = 0.0; //igaühel eraldi palgafond
-    Double koguFond = 0.0; //summaarne palgafond ettevõtjale
+    Double fondid = 0.0; //Igaühel eraldi palgafond
+    Double koguFond = 0.0; //Summaarne palgafond ettevõtjale
 
-    //listid muutujate jaoks
-/*Näidis saadud Marko Esna tööst**/
+    //Listid muutujate jaoks
+
+/*@Author Marko Esna - näidis**/
     ArrayList<String> nimed = new ArrayList<>();
     ArrayList<Double> netod = new ArrayList<>();
     ArrayList<Double> brutod = new ArrayList<>();
@@ -23,92 +24,107 @@ public class Funktsioonid extends Main {
 
 
     //Funktsioon väärtuste listi lisamiseks
+
     public void lisaListi(String nimi, Double neto, Double bruto){
 
-
-/*Näidis saadud Marko Esna tööst**/
-        nimed.add(nimi); //uue inimese nime listi järgmisele kohale lisamine
-        netod.add(neto); //uue inimese netopalga listi järgmisele kohale lisamine
-        brutod.add(bruto); //uue inimese brutopalga listi järgmisele kohale lisamine
-        palgafondid.add(fondid); //uue inimese palgafondi listi järgmisele kohale lisamine
+/*@Author Marko Esna - näidis**/
+        nimed.add(nimi); //Lisab uue inimese nime listi järgmisele kohale
+        netod.add(neto); //Lisab uue inimese netopalga listi järgmisele kohale
+        brutod.add(bruto); //Lisab uue inimese brutopalga listi järgmisele kohale
+        palgafondid.add(fondid); //Lisab uue inimese palgafondi listi järgmisele kohale
     }
 
 
     //Muudab nimed Camel Case'ile vastavaks (esimene täht igas sõnas suurtäht), nagu eesti keele reeglitele kohane.
-/*@Author http://www.javawithus.com/programs/converting-string-to-different-cases-upper-lower-toggle-camel-sentence-case **/
+
+/*@Author Java With Us
+* http://www.javawithus.com/programs/converting-string-to-different-cases-upper-lower-toggle-camel-sentence-case
+**/
+
     public static String toCamelCase(String inputString) {
-        String result = ""; //loon muutuja
-        if (inputString.length() == 0) { //kui sisendi pikkus on 0, siis jääb nii, nagu on
-            return result; //väljastatakse tulemus juhul, kui sisend on tühi
+        String result = ""; //Loob muutuja
+        if (inputString.length() == 0) { //Kui sisendi pikkus on 0, siis jääb nii, nagu on
+            return result; //Väljastab tulemuse juhul, kui sisend on tühi
         }
-        char firstChar = inputString.charAt(0); //loon muutuja, mis võtab sisendi esimese märgi
-        char firstCharToUpperCase = Character.toUpperCase(firstChar);
-        result = result + firstCharToUpperCase;
-        for (int i = 1; i < inputString.length(); i++) { //for tsükkel märkide 1 kuni i läbikäimiseks
-            char currentChar = inputString.charAt(i);  // määrab ära, milline märk on hetkel käsil
-            char previousChar = inputString.charAt(i - 1); //määrab ära eelmise märgi
-            if (previousChar == ' ') {  //if lause, "kui eelmine märk on tühik"
-                char currentCharToUpperCase = Character.toUpperCase(currentChar); //siis praegune muudetakse suureks täheks
-                result = result + currentCharToUpperCase; //tulemusele liidetakse suur märk
+        char firstChar = inputString.charAt(0); //Loob muutuja, mis võtab sisendi esimese märgi
+        char firstCharToUpperCase = Character.toUpperCase(firstChar); //Muudab esimese tähe suurtäheks
+        result = result + firstCharToUpperCase; //Lisab esitähe tulemusele
+        for (int i = 1; i < inputString.length(); i++) { //For tsükkel märkide 1 kuni i läbikäimiseks
+            char currentChar = inputString.charAt(i);  // Määrab ära, milline märk on hetkel käsil
+            char previousChar = inputString.charAt(i - 1); //Määrab ära eelmise märgi
+            if (previousChar == ' ') {  //If lause, "kui eelmine märk on tühik", siis ...
+                char currentCharToUpperCase = Character.toUpperCase(currentChar); //Praeguse märgi muudab suureks täheks
+                result = result + currentCharToUpperCase; //Tulemusele liidab suure märgi
             } else {
-                char currentCharToLowerCase = Character.toLowerCase(currentChar); //muul juhul muudetakse väikeseks märgiks
-                result = result + currentCharToLowerCase; //ja liidetakse tulemusele
+                char currentCharToLowerCase = Character.toLowerCase(currentChar); //Muul juhul muudab väikeseks märgiks
+                result = result + currentCharToLowerCase; //Liidetakse tähe tulemusele
             }
         }
-        return result; //väljastatakse tulemus
+        return result; //Väljastab tulemuse
     }
 
 
     //Funktsioon netode, brutode ja palgafondide leidmiseks ning info väljaprintimiseks.
+
     public void fondiArvutused(){
-        DecimalFormat f = new DecimalFormat("##.00"); //muudab 0,xxxxxxx... numbrid 0,xx-ks.
+        DecimalFormat f = new DecimalFormat("##.00"); //Muudab 0,xxxxxxx... numbrid 0,xx-ks.
 
-        //for tsükli abil teeg kõik funktsiooni astmed läbi kõigi massiivi liikmetega lähtudes nimede massiivi liikmetest
+
          for (int i = 0; i < nimed.size(); i++) {
+             //For tsükli abil teeb kõik funktsiooni astmed läbi kõigi massiivi liikmetega lähtudes nimede massiivi liikmetest
 
-             //eemaldan veidrad märgid nimedest, kui midagi on sinna sattunud
+
+
              String susi=nimed.get(i).replaceAll("[0-9\\+*/_<>=?!#%:\\.;,\\)\\(\\´\\{\\}\"¤&`@£$€§½]+", "");
-             //nende märkite eemaldamisega on probleeme: []\|
+             //Eemaldab veidrad märgid nimedest, kui midagi on sinna sattunud
+             //Märkide []\| eemaldamisega on probleeme
 
-             nimed.set(i, toCamelCase(susi)); //muudan nimed Camel Case tekstiks
+             nimed.set(i, toCamelCase(susi)); //Muudab nimed Camel Case tekstiks toCamelCase meetodi abil
 
-             if (brutod.get(i) > 0 && netod.get(i)==0) { //kui netot ei tea, aga bruto on olemas
-                fondid = brutod.get(i)*(1.384); //arvutab brutost palgafondi
-                neto = brutod.get(i)/1.2; //arvutab brutost neto
+             if (brutod.get(i) > 0 && netod.get(i)==0) { //Kui netot ei tea, aga bruto on olemas, siis ...
+                fondid = brutod.get(i)*(1.384); //Arvutab brutost palgafondi
+                neto = brutod.get(i)/1.2; //Arvutab brutost neto
                 //33%=sotsmaksud, 1,8% töötuskindlustusmaks ettevõttele, 1,6% töötuskindlustusmaks töötajale, 2% 2. pensionisammas
-                //33+1,8+1,6+2=38,4%
-                netod.set(i, neto); //viib uue neto õigele kohale netode massiivi
-                palgafondid.set(i, fondid); //viib saadud palgafondi õigesse kohta palgafondi massiivi
+                //33+1,8+1,6+2=38,4% -> 1,384
+                netod.set(i, neto); //Viib uue neto õigele kohale netode massiivi
+                palgafondid.set(i, fondid); //Viib saadud palgafondi õigesse kohta palgafondi massiivi
                 System.out.println( nimed.get(i) + " neto: " + f.format(netod.get(i)) + " bruto: " + f.format(brutod.get(i))+ ", palgafond: " + f.format(palgafondid.get(i)));
+                //Trükib kõik andmed välja ühes reas üksteise järel täpsusega kaks kohta pärast koma.
             }
-            else if (brutod.get(i) == 0  && netod.get(i) > 0){ //kui brutot ei tea, aga neto on olemas
-                fondid = netod.get(i)*1.2*(1.384); //arvutab netost palgafondi
+            else if (brutod.get(i) == 0  && netod.get(i) > 0){ //Kui brutot ei tea, aga neto on olemas, siis ...
+                fondid = netod.get(i)*1.2*(1.384); //Arvutab netost palgafondi
                 //33%=sotsmaksud, 1,8% töötuskindlustusmaks ettevõttele, 1,6% töötuskindlustusmaks töötajale, 2% 2. pensionisammas, 20% tulumaksu
-                bruto = netod.get(i)*1.2; //arvutab netost bruto
-                brutod.set(i, bruto); //viib uue bruto õigele kohale brutode massiivi
-                palgafondid.set(i, fondid); //viib saadud palgafondi õigesse kohta palgafondi massiivi
+                bruto = netod.get(i)*1.2; //Arvutab netost bruto
+                brutod.set(i, bruto); //Viib uue bruto õigele kohale brutode massiivi
+                palgafondid.set(i, fondid); //Viib saadud palgafondi õigesse kohta palgafondi massiivi
                 System.out.println( nimed.get(i) + " neto: " + f.format(netod.get(i)) + " bruto: " + f.format(brutod.get(i))+ ", palgafond: " + f.format(palgafondid.get(i)));
-            }
+                 //Trükib kõik andmed välja ühes reas üksteise järel täpsusega kaks kohta pärast koma.
+             }
             else {System.out.println("Vigane rida: neto = " + netod.get(i) + ", bruto = "+ brutod.get(i) + ".");}
-            //kui ei teata ei netot ega brutot või on sisestatud arv väiksem nullist
+            //Kui ei teata ei netot ega brutot või on sisestatud arv väiksem nullist, siis trükib selle rea täpsusega kaks kohta pärast koma
 
-            if (brutod.get(i) >=0 && (netod.get(i)) >=0 ){ //kui vaadeldavad brutod ja netod on nullist suuremad, siis
-                System.out.println("Palgafond kasutajal " + nimed.get(i)+  " on " + f.format(palgafondid.get(i)) + " eurot."); //trükitakse selline teks
-                koguFond = koguFond + palgafondid.get(i);//ja liidetakse palgafond kogufondile juurde
-                System.out.println("Aasta palgafond kasutajal " + nimed.get(i)+  " on " + f.format(palgafondid.get(i)*12) + " eurot."); //leitakse aastafond ja trükitakse välja
+            if (brutod.get(i) >=0 && (netod.get(i)) >=0 ){ //Kui vaadeldavad brutod ja netod on nullist suuremad, siis ...
+                System.out.println("Palgafond kasutajal " + nimed.get(i)+  " on " + f.format(palgafondid.get(i)) + " eurot.");
+                //Trükib sellise teksti täpsusega kaks kohta pärast koma
+                koguFond = koguFond + palgafondid.get(i); //Liidab saadud palgafondi kogufondile juurde
+                System.out.println("Aasta palgafond kasutajal " + nimed.get(i)+  " on " + f.format(palgafondid.get(i)*12) + " eurot.");
+                //Leiab aastafondi ja trükib välja
             }
-            else continue; //muidu jäetakse rida vahele
+            else continue; //Muul juhul jätab rea vahele ja liigub järgmise juurde
 
         }
     }
 
+    //Meetod muutab numbrid täpsusele 2 kohta pärast koma ja trükib välja erinevad tekstid.
 
     public void lopp() {
-        DecimalFormat f = new DecimalFormat("##0.00"); //muudab 0,xxxxxxx... numbrid 0,xx-ks.
+        DecimalFormat f = new DecimalFormat("##0.00"); //Muudab 0,xxxxxxx... kujul numbrid 0,xx-ks.
         System.out.println("Kogu palgafond kuus on " + f.format(koguFond) + " eurot.");
-        System.out.println("[Nimi, neto, bruto, palgafond]");
-        for (int i = 0; i < nimed.size(); i++){ //for tsükli abil trükib ekraanile andmed maatriksi kujul
+        //Trükib ettevõtte kogu ühe kuu palgafondi
+        System.out.println("[Nimi, neto, bruto, palgafond]"); //Trükib tabeli esimese rea
+        for (int i = 0; i < nimed.size(); i++){ //For tsükli abil trükib ekraanile andmed nimede massiivi järjekorra alusel
             System.out.println("[" + nimed.get(i) + "; " + f.format(netod.get(i)) + "; " + f.format(brutod.get(i)) + "; "+ f.format(palgafondid.get(i)) + " ]");
+            //Trükib igast massiivist sama järjekorranumbriga andmed üksteise all tabelisse
         }
     }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,83 +16,91 @@ import javafx.scene.layout.Pane;
 *@since 1.8
 **/
 
-//Valdav osa muude märgeteta koodist sellel lehel saadud lehelt http://i200.itcollege.ee/#Kasutajaliides ja modifitseeritud minu poolt projektile sobivaks
-
+/*Valdav osa muude märgeteta koodist sellel lehel saadud lehelt
+* http://i200.itcollege.ee/#Kasutajaliides
+* ja modifitseeritud minu poolt projektile sobivaks
+**/
 
 public class Main extends Application {
 
-/*Rida saadud Hardi Tiituselt ja Marko Esna tööst **/
+/*@Author Hardi Tiitus ja Marko Esna - 1 rida**/
     @Override
 
-/*Rida saadud Marko Esna tööst **/
     public void start(Stage primaryStage) throws Exception{
 
-        Funktsioonid func = new Funktsioonid(); //funktsioonide listi loomine
-        Pane paan = new Pane();   //Loon uue paani
-        Scene stseen = new Scene(paan, 375, 425); //akna kõrgus ja laius
+/*@Author Marko Esna - 1 rida **/
+        Funktsioonid func = new Funktsioonid(); //Loob funktsioonide listi
+        Pane paan = new Pane();   //Loob uue paani
+        Scene stseen = new Scene(paan, 375, 425); //Loob uue stseeni, määrab selle kõrguse ja laiuse
         primaryStage.setScene(stseen);
         primaryStage.show();
-        primaryStage.setTitle("Palgaarvestus"); //Akna pealkiri
+        primaryStage.setTitle("Palgaarvestus"); //Määrab aknale pealkirja
+
 //Abi saadud:  lehelt http://i200.itcollege.ee/#Kasutajaliides ja Külli Ristilt
 
-        VBox vbox = new VBox(2); //2-pikslise vahega kastid üksteise alla
-        vbox.setAlignment(Pos.BOTTOM_CENTER); //ei tea täpselt, mida see teeb, sest ei näe seda veel
-        vbox.setTranslateY(100); //nihutas kastid ja teksti ülemisest äärest eemale
-        vbox.setTranslateX(100); //nihutas kastid ja teksti vasakust äärest eemale
+        VBox vbox = new VBox(2); //Loob 2-pikslise vahega kastid üksteise alla
+        vbox.setAlignment(Pos.BOTTOM_CENTER); //Joondab kastide kohal oleva teksti kastide keskele
+        vbox.setTranslateY(100); //Nihutab kastid ja teksti ülemisest äärest eemale
+        vbox.setTranslateX(100); //Nihutab kastid ja teksti vasakust äärest eemale
 
 
-        //Lahtrid teksti ja numbrite sisestuseks
-/*Esimene rida saadud Külli Ristilt **/
-        Label nimiVali = new Label("Sisesta töötaja nimi:"); //rida enne sisestuskastikest
-        TextField nimi = new TextField();      //küsib tekstilisi andmeid
-        nimi.setPromptText("sisesta nimi");   //see on salajane tekst
+        //Loob lahtrid teksti ja numbrite sisestuseks
+/*@Author Külli Ristilt  - 1 rida **/
+        Label nimiVali = new Label("Sisesta töötaja nimi:"); //Loob ja sisestab rea enne sisestuskastikest
+        TextField nimi = new TextField();      //Loob tekstikasti
+        nimi.setPromptText("sisesta nimi");   //loob informatiivse teksti tekstikasti
 
 
 
-        Label netoVali = new Label("Sisesta töötaja netopalk:"); //rida enne sisestuskastikest
-        TextField neto = new TextField();          //küsib tekstilisi andmeid
-        neto.setPromptText("sisesta netopalk");  //see on salajane tekst
+        Label netoVali = new Label("Sisesta töötaja netopalk:"); //Loob ja sisestab rea enne sisestuskastikest
+        TextField neto = new TextField();          //Loob tekstikasti
+        neto.setPromptText("sisesta netopalk");  //loob informatiivse teksti tekstikasti
 
-        Label brutoVali = new Label("Sisesta töötaja brutopalk:"); //rida enne sisestuskastikest
-        TextField bruto = new TextField();      //küsib tekstilisi andmeid
-        bruto.setPromptText("sisesta brutopalk");   //see on salajane tekst
+        Label brutoVali = new Label("Sisesta töötaja brutopalk:"); //Loob ja sisestab rea enne sisestuskastikest
+        TextField bruto = new TextField();      //Loob tekstikasti
+        bruto.setPromptText("sisesta brutopalk");   //loob informatiivse teksti tekstikasti
 
-        //nupud sisestuse, arvutamise ja puhastamise tarbeks
-/*Funktsiooni kasutamise ülesehitus saadud Marko Esna tööst ja ümber tehtud minu poolt **/
-        Button sisesta = new Button("Sisesta"); //1. nupp, sisestamiseks
-        sisesta.setOnAction(event -> {
-            func.lisaListi( nimi.getText(), parsiSisend(neto.getText()), parsiSisend(bruto.getText())); //andmete sisestus
-            // Double-iks tegemise errori ja 0 sisestamise vajaduse eemaldamiseks kasutan ka parsiSisend meetodid netol ja brutol
-            nimi.clear(); neto.clear(); bruto.clear(); //välja tühjendamine
+        //Loob nupud sisestuse, arvutamise ja puhastamise tarbeks koos lisatud funktsioonidega
+
+/*@Author Marko Esna - funktsiooni kasutamise ülesehitus, mis minu poolt ümber tehtud**/
+        Button sisesta = new Button("Sisesta"); //Loob "Sisesta" nupu teksti sisestamise kinnitamiseks
+        sisesta.setOnAction(event -> { //Nupuga kaasnevad funktsioonid
+            func.lisaListi( nimi.getText(), parsiSisend(neto.getText()), parsiSisend(bruto.getText())); //Sisestab andmed
+            // Double-iks tegemise errori ja 0 sisestamise vajaduse eemaldamiseks kasutab parsiSisend meetodit netol ja brutol
+            nimi.clear(); neto.clear(); bruto.clear(); //Tühjendab väljad
         });
 
-        Button arvuta = new Button("Arvuta tulemus"); //2. nupp töötlemiseks
-        arvuta.setOnAction(event -> {
-            func.fondiArvutused(); // funktsiooni tulemuse arvutamine ja esitamine
-            nimi.clear(); neto.clear(); bruto.clear(); // arvutusmälu tühjendamine
+        Button arvuta = new Button("Arvuta tulemus"); //Loob "Arvuta tulemus" nupu teksti sisestamise kinnitamiseks
+        arvuta.setOnAction(event -> { //Nupuga kaasnevad funktsioonid
+            func.fondiArvutused(); //Arvutab ja esitab funktsiooni tulemused
+            nimi.clear(); neto.clear(); bruto.clear(); //Tühjendab arvutusmälu
         });
 
-        Button lopeta = new Button("Lõpeta arvutused"); //3. nupp väärtuste tühjendamiseks mälust ja lõpptulemuse esitamine
-        lopeta.setOnAction(event -> {func.lopp();
-            nimi.clear(); neto.clear(); bruto.clear(); //mälu tühjendamine
+        Button lopeta = new Button("Lõpeta arvutused"); //Loob "Lõpeta arvutused" nupu lõpptulemuste saamiseks
+        lopeta.setOnAction(event -> {  //Nupuga kaasnevad funktsioonid
+            func.lopp(); //Teen lõpparvutused ja väljastab koondandmed
+            nimi.clear(); neto.clear(); bruto.clear(); //Tühjendab arvutusmälu
         });
 
-        // kõigi valitud nuppude ja väljade näitamine
         vbox.getChildren().addAll(nimiVali, nimi, netoVali, neto, brutoVali, bruto, sisesta, arvuta, lopeta);
-        // kõigi valitud nuppude ja väljade näitamine
+        // Valib kõiki nupud ja väljad
         paan.getChildren().add(vbox);
+        // Näitab kõiki valitud nuppe ja välju
     }
+
+    //Meetod, et katsetada, kas neto ja bruto on puhtad numbrid või ei ole ning vastavalt neid muuta
+
 /*@Author Kristel Meikas**/
-    private Double parsiSisend(String sisend) { //funktsioon/meetod, et katsetada, kas on puhas number või ei
-        //private - saab kasutada ainult sama faili sees, public - saab kasutada ka mujalt
-        try { //try-catch katsetus
-            if (!sisend.isEmpty()) { //kui ei ole tühi
-                return Double.parseDouble(sisend); //siis võib parsida Double'iks
+    private Double parsiSisend(String sisend) {
+        //Private - saab kasutada ainult sama faili sees, public - saab kasutada ka mujalt
+        try { //Try-catch katsetus
+            if (!sisend.isEmpty()) { //Kui ei ole tühi, siis ...
+                return Double.parseDouble(sisend); //Parsib Double'iks
             }
-        } catch (NumberFormatException e) { //muul juhul annaks errorid, kuid eemaldasime ülearuse rea
-            System.out.println("Sisendväärtuse formaat ei sobi, sisend = " + sisend); //errori asemel tekst ja väljund
-            //e.printStackTrace(); //errorite väljastamise rida, mille eemaldasime
+        } catch (NumberFormatException e) { //Muul juhul püüab vead kinni
+            System.out.println("Sisendväärtuse formaat ei sobi, sisend = " + sisend); //Väljastab tekst ja sisendi
+            //e.printStackTrace(); //Vigade väljastamise rida, mille eemaldasin, et puhtamana hoida
         }
-        return 0.0; //muul juhul annab 0.0 ehk double vormingus 0
+        return 0.0; //Muul juhul annab 0.0 ehk double vormingus 0
     }
 }
